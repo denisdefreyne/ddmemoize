6692226db524a32dffd87c1eaf50307ecb14c87f
Denis Defreyne <denis.defreyne@stoneship.org>
2017-12-01 21:41:56 +0100

    Remove source code documentation

--- lib/ddmemoize.rb
+++ lib/ddmemoize.rb
@@ -4,9 +4,7 @@ require 'ref'
 
 require_relative 'ddmemoize/version'
 
-# Adds support for memoizing functions.
 module DDMemoize
-  # @api private
   class Value
     attr_reader :value
 
@@ -21,39 +19,7 @@ module DDMemoize
     mod.extend(Mixin)
   end
 
-  # @api private
   module Mixin
-    # Memoizes the method with the given name. The modified method will cache
-    # the results of the original method, so that calling a method twice with
-    # the same arguments will short-circuit and return the cached results
-    # immediately.
-    #
-    # Memoization assumes that the current object as well as the function
-    # arguments are immutable. Mutating the object or the arguments will not
-    # cause memoized methods to recalculate their results. There is no way to
-    # un-memoize a result, and calculation results will remain in memory even
-    # if they are no longer needed.
-    #
-    # @example A fast fib function due to memoization
-    #
-    #     class FibFast
-    #       extend Denis::Memoize
-    #
-    #       def run(n)
-    #         if n == 0
-    #           0
-    #         elsif n == 1
-    #           1
-    #         else
-    #           run(n-1) + run(n-2)
-    #         end
-    #       end
-    #       memoize :run
-    #     end
-    #
-    # @param [Symbol, String] method_name The name of the method to memoize
-    #
-    # @return [void]
     def memoize(method_name)
       original_method_name = '__nonmemoized_' + method_name.to_s
       alias_method original_method_name, method_name
